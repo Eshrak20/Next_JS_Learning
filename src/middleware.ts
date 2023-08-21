dd4cc03a457f1d4fbbd18e3c09f72c358fe2e79a
Eshrak <uemran2018@gmail.com>
2023-08-21 07:51:15 +0600

    added middle ware

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { log } from 'console'
+import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
+ 
+export function middleware(request: NextRequest) {
+   
+    const path = request.nextUrl.pathname
+
+    const isPublicPath = path === '/login' || path === '/signup'
+
+    const token  = request.cookies.get('token')?.value || ''
+    console.log('this is token',token , 'this is isPublicPath',isPublicPath);
+    
+
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+        
+    }
+
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher:  ['/','/profile','/login','/signup']
+  
+}
